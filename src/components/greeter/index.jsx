801ec1f5adb29869a6a1048689b1d0eb641f9307
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2016-07-12 13:10:53 -0400

    Update demo index page

--- src/components/greeter/index.jsx
+++ src/components/greeter/index.jsx
@@ -1,12 +1,34 @@
-import './styles'
+import './styles';
 
-export const Greeter = ({ name }) =>
-  <div className='greeter'>
-    <h1>Hello, {name}.</h1>
-  </div>
+export class Greeter extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: props.name
+    };
+
+    this.updateText = this.updateText.bind(this);
+  }
+
+  updateText(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className='greeter'>
+        <h1>Hello, {this.state.name}.</h1>
+        <input name="name" value={this.state.name} onChange={this.updateText} />
+        &nbsp;
+        <label htmlFor="name">(Change me...)</label>
+      </div>
+    );
+  }
+}
 
 Greeter.propTypes = {
   name: React.PropTypes.string
-}
+};
 
-export default Greeter
+export default Greeter;
